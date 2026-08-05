ce30f8f261fdb98e64c0be11624398dcf89cb384
isabeleeduardaoliveiraborges <isabeleeduarda87@gmail>
2026-08-05 17:48:44 -0300

    aula02bd

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
-app=FastAPI(title="minha primeira API")
-@app.get('/')
-def principal():
-    return{'mensagem':'minha primeira API em FastAPI!'}
+# main.py
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import Base, engine, get_db
+from models import ProdutoDB
+from schemas import ProdutoCreate, ProdutoResponse
+Base.metadata.create_all(bind=engine) # cria as tabelas, se ainda não existirem
+app = FastAPI()
 
-@app.get('/sobre')
-def sobre():
-    return{'mensagem':'pagina sobre'}
+@app.get('/produtos', response_model=list[ProdutoResponse])
+def listar_produtos(db: Session = Depends(get_db)):
+    return db.query(ProdutoDB).all()
+
+@app.post('/produtos', response_model=ProdutoResponse, status_code=201)
+def criar_produto(produto: ProdutoCreate, db: Session = Depends(get_db)):
+    novo_produto = ProdutoDB(**produto.dict())
+    db.add(novo_produto)
+    db.commit()
+    db.refresh(novo_produto)
+    return novo_produto
